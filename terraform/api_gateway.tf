550bf4a8aa9822056b6737064f947eebfa5c3855
koki-develop <kou.pg.0131@gmail.com>
2022-05-28 17:00:30 +0900

    passthrough_behavior, request_templates.application/json を設定

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -10,10 +10,15 @@ resource "aws_api_gateway_method" "get_ip" {
 }
 
 resource "aws_api_gateway_integration" "get_ip" {
-  rest_api_id = aws_api_gateway_rest_api.checkip.id
-  resource_id = aws_api_gateway_rest_api.checkip.root_resource_id
-  http_method = aws_api_gateway_method.get_ip.http_method
-  type        = "MOCK"
+  rest_api_id          = aws_api_gateway_rest_api.checkip.id
+  resource_id          = aws_api_gateway_rest_api.checkip.root_resource_id
+  http_method          = aws_api_gateway_method.get_ip.http_method
+  type                 = "MOCK"
+  passthrough_behavior = "WHEN_NO_TEMPLATES"
+
+  request_templates = {
+    "application/json" : jsonencode({ statusCode : 200 })
+  }
 }
 
 resource "aws_api_gateway_method_response" "get_ip_200" {
