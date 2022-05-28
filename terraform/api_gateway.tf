79ecfd6095276cc9cc7d9a929fa36fe35b4294a8
koki-develop <kou.pg.0131@gmail.com>
2022-05-28 16:56:30 +0900

    integration response を作成

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -22,3 +22,14 @@ resource "aws_api_gateway_method_response" "get_ip_200" {
   http_method = aws_api_gateway_method.get_ip.http_method
   status_code = "200"
 }
+
+resource "aws_api_gateway_integration_response" "get_ip_200" {
+  rest_api_id = aws_api_gateway_rest_api.checkip.id
+  resource_id = aws_api_gateway_rest_api.checkip.root_resource_id
+  http_method = aws_api_gateway_method.get_ip.http_method
+  status_code = aws_api_gateway_method_response.get_ip_200.status_code
+
+  response_templates = {
+    "application/json" = "test"
+  }
+}
