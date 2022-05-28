c326d7cdb66093bb40281bbda13c73306b3416b6
koki-develop <kou.pg.0131@gmail.com>
2022-05-28 16:46:57 +0900

    mock integration を設定

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -8,3 +8,10 @@ resource "aws_api_gateway_method" "get_ip" {
   http_method   = "GET"
   authorization = "NONE"
 }
+
+resource "aws_api_gateway_integration" "get_ip" {
+  rest_api_id = aws_api_gateway_rest_api.checkip.id
+  resource_id = aws_api_gateway_rest_api.checkip.root_resource_id
+  http_method = aws_api_gateway_method.get_ip.http_method
+  type        = "MOCK"
+}
