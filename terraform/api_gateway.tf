99ebba1c43f91e9790eaf01d31c78ac8be8c9d49
koki-develop <kou.pg.0131@gmail.com>
2022-05-28 16:52:38 +0900

    メソッドレスポンスを作成

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -15,3 +15,10 @@ resource "aws_api_gateway_integration" "get_ip" {
   http_method = aws_api_gateway_method.get_ip.http_method
   type        = "MOCK"
 }
+
+resource "aws_api_gateway_method_response" "get_ip_200" {
+  rest_api_id = aws_api_gateway_rest_api.checkip.id
+  resource_id = aws_api_gateway_rest_api.checkip.root_resource_id
+  http_method = aws_api_gateway_method.get_ip.http_method
+  status_code = "200"
+}
