0168c6512bcd3e0338c55a502191cabb48d8c15e
koki-develop <kou.pg.0131@gmail.com>
2022-05-28 16:46:46 +0900

    method を作成

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -1,3 +1,10 @@
 resource "aws_api_gateway_rest_api" "checkip" {
   name = "checkip"
 }
+
+resource "aws_api_gateway_method" "get_ip" {
+  rest_api_id   = aws_api_gateway_rest_api.checkip.id
+  resource_id   = aws_api_gateway_rest_api.checkip.root_resource_id
+  http_method   = "GET"
+  authorization = "NONE"
+}
