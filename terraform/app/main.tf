4e13c8fdc6befdab9f808222c34c0f5c2be2903e
koki-develop <kou.pg.0131@gmail.com>
2022-07-18 15:49:35 +0900

    profile の設定を削除

--- terraform/app/main.tf
+++ terraform/app/main.tf
@@ -1,8 +1,7 @@
 terraform {
   backend "s3" {
-    bucket  = "checkip-tfstates"
-    region  = "us-east-1"
-    key     = "app/terraform.tfstate"
-    profile = "default"
+    bucket = "checkip-tfstates"
+    region = "us-east-1"
+    key    = "app/terraform.tfstate"
   }
 }
