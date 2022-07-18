b82971938b8eff4b6b17a4406e44e507ef9a88b1
koki-develop <kou.pg.0131@gmail.com>
2022-07-18 15:55:30 +0900

    github actions 用の iam role を作成

---
+++ terraform/action/iam.tf
@@ -0,0 +1,42 @@
+data "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+}
+
+resource "aws_iam_role" "action" {
+  name               = "${local.prefix}-action-role"
+  assume_role_policy = data.aws_iam_policy_document.action_assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "action_assume_role_policy" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    principals {
+      type        = "Federated"
+      identifiers = [data.aws_iam_openid_connect_provider.github.arn]
+    }
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:koki-develop/checkip:*"]
+    }
+  }
+}
+
+resource "aws_iam_policy" "action" {
+  name   = "${local.prefix}-action-policy"
+  policy = data.aws_iam_policy_document.action_policy.json
+}
+
+data "aws_iam_policy_document" "action_policy" {
+  statement {
+    effect    = "Allow"
+    actions   = ["*"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "action" {
+  role       = aws_iam_role.action.name
+  policy_arn = aws_iam_policy.action.arn
+}
