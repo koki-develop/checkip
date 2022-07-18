9e0ad59f81e2a4bbe51482a4f4ad9763ac728e3a
koki-develop <kou.pg.0131@gmail.com>
2022-07-18 15:47:54 +0900

    tfstate のパスを修正

--- terraform/app/main.tf
+++ terraform/app/main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket  = "checkip-tfstates"
     region  = "us-east-1"
-    key     = "terraform.tfstate"
+    key     = "app/terraform.tfstate"
     profile = "default"
   }
 }
